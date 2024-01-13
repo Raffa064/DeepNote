448969141718f4c783808daf7a91de2ec943e52b
Raffa064 <raffaddtg@gmail.com>
2024-01-12 21:11:51 -0300

    Full refactory: DeepNote core

--- js/deep-note.js
+++ js/deep-note.js
@@ -1,11 +1,188 @@
-const DN_CORE_VERSION = 1;
-
 DeepNote = (() => {
-  if (localStorage.dnCoreVersion === undefined) {
-    localStorage.dnCoreVersion = DN_CORE_VERSION;
+  const data = loadData();
+
+  function loadData() {
+    const dataJson = localStorage.dn_data; // Workspce list
+
+    if (dataJson !== undefined) {
+      const dataObj = JSON.parse(dataJson);
+
+      if (dataObj.length !== undefined) {
+        return dataObj;
+      }
+
+      throw new Error("Invalid dn_data structure.");
+    }
+
+    return [];
   }
 
-  applyUpdatePatchs();
+  function save() {
+    const dataJson = JSON.stringify(data, (key, value) => {
+      return key === "root" ? undefined : value; // Prevent circular structure
+    });
+
+    localStorage.dn_data = dataJson;
+  }
+
+  function createCard(checked, title, description, children) {
+    var card;
+
+    if (typeof checked === "object") {
+      card = checked;
+    } else {
+      card = {
+        checked,
+        title,
+        description,
+        children,
+        parent: null,
+      };
+    }
+
+    card.setChecked = (checked) => {
+      if (!card.hasChildren()) {
+        return (card.checked = checked);
+      }
+
+      return card.isChecked();
+    };
+
+    card.isChecked = () => {
+      const verifyChildrenChecked = () => {
+        const uncheckedChild = card.children.find((childCard) => {
+          return childCard.checked === false;
+        });
+
+        return uncheckedChild !== undefined;
+      };
+
+      return !card.hasChildren() ? card.checked : verifyChildrenChecked();
+    };
+
+    card.setTitle = (title) => {
+      return (card.title = title);
+    };
+
+    card.getTitle = () => {
+      return card.title;
+    };
+
+    card.setDescription = (description) => {
+      return (card.description = description);
+    };
+
+    card.getDescription = () => {
+      return card.description;
+    };
+
+    card.hasChildren = () => {
+      return card.children.length > 0;
+    };
+
+    card.getChildren = () => {
+      return card.children;
+    };
+
+    card.addChild = (childCard) => {
+      childCard.parent = this;
+      card.children.push(childCard);
+    };
+
+    card.removeChild = (childCard) => {
+      childCard.parent = null;
+
+      const index = card.children.indexOf(childCard);
+      card.children.splice(index, 1);
+
+      return childCard;
+    };
+
+    card.json = () => {
+      return JSON.stringify(card, (key, value) => {
+        return key === "parent" ? undefined : value; // Prevent circular structure
+      });
+    };
+
+    card.children = card.children.map((childCard) => {
+      createCard(childCard); // inject functions
+      childCard.parent = card;
+    });
+
+    return card;
+  }
+
+  function createWorkspace(name) {
+    var workspace;
+
+    if (typeof name === "string") {
+      const root = createCard(false, "", "", []);
+      workspace = {
+        name,
+        content: root.json(),
+      };
+    } else {
+      workspace = name;
+    }
+
+    workspace.save = () => {
+      const index = data.indexOf(workspace);
+
+      if (index < 0) {
+        data.push(workspace); // add to workspace list
+      }
+
+      save(); // save state
+    };
+
+    workspace.delete = () => {
+      const index = data.indexOf(workspace);
+
+      if (index >= 0) {
+        data.splice(index, 1); // delete from workspace list
+        save(); // save state
+      }
+    };
+
+    workspace.json = () => {
+      return JSON.stringify(workspace);
+    };
+
+    return workspace;
+  }
+
+  function loadWorkspace(name) {
+    const workspace = data.find((w) => w.name === name);
+
+    if (workspace !== undefined) {
+      const rootJson = workspace.content;
+      const rootParsed = JSON.parse(rootJson);
+      const root = createCard(rootParsed);
+
+      workspace.root = root;
+      return createWorkspace(workspace); // inject functions
+    }
+
+    throw new Error("Trying to load unnexisting workspace '" + name + "'");
+  }
+
+  function getWorkspaceNames() {
+    const nameList = data.map((workspace) => {
+      return workspace.name;
+    });
+
+    return nameList;
+  }
+
+  return {
+    createCard,
+    createWorkspace,
+    loadWorkspace,
+    getWorkspaceNames,
+  };
+})();
+
+/*DeepNote = (() => {
   const deep_note = load();
 
   function load() {
@@ -33,7 +210,7 @@ DeepNote = (() => {
   }
 
   function createCard(checked, title, description, children) {
-    if (title == undefined) {
+    if (title === undefined) {
       const card = checked; // JSON-Parsed card, without functions
 
       const _checked = card.checked;
@@ -103,7 +280,8 @@ DeepNote = (() => {
     workspace.save = () => {
       workspace.content = root.json();
 
-      if (!deep_note.find((w) => w.name === workspace.name)) {
+      const exists = deep_note.find((w) => w.name === workspace.name);
+      if (!exists) {
         deep_note.push(workspace);
       }
 
@@ -160,16 +338,4 @@ DeepNote = (() => {
     loadWorkspace,
     getWorkspaceNames,
   };
-})();
-
-function applyUpdatePatchs() {
-  var version = parseInt(localStorage.dnCoreVersion);
-
-  if (version < 1 || version > DN_CORE_VERSION) {
-    throw new Error("Invalid core version");
-  }
-
-  // Apply version patchs
-
-  localStorage.dnCoreVersion = version;
-}
+})();*/
